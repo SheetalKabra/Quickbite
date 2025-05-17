cab1fb33e9afc1a85de8b2c481cbbb75600b32e4
Sheetal Kabra <sheetalkabra4@gmail.com>
2025-05-17 13:10:16 +0530

    Frontend: add Dashboard, Header, and Footer in the App.js

--- frontend/quickbite-frontend/src/App.js
+++ frontend/quickbite-frontend/src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './components/Register';
 import Login from './components/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
 
 
 function App() {
     return (
         <Router>
+        <Header></Header>
         <div style={{ minHeight: "80vh", paddingTop: "70px", paddingBottom: "50px" }}>
         <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Dashboard />} />
         </Routes>
         </div>
-    {/* <Footer /> */}
+    <Footer />
     </Router>
     );
 }
