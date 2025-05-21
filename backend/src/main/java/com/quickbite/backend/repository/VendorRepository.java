5a41245645b069f5b031ffbd2d2a4cbfa6a86f39
Sheetal Kabra <sheetalkabra4@gmail.com>
2025-05-21 11:48:42 +0530

    Backend: add api to get vendor by category id

--- backend/src/main/java/com/quickbite/backend/repository/VendorRepository.java
+++ backend/src/main/java/com/quickbite/backend/repository/VendorRepository.java
@@ -4,6 +4,9 @@ import com.quickbite.backend.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    List<Vendor> findVendorByCategoryId(Long categoryId);
 }
